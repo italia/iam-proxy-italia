4ab80283f58af9fa177c7959a881e12db32da3fa
MDreW <andrea.ranaldi@gmail.com>
2025-08-06 17:27:02 +0200

    cert-name set as satosa

--- Docker-compose/nginx/certbot.sh
+++ Docker-compose/nginx/certbot.sh
@@ -1,6 +1,7 @@
 if [[ "$CERTBOT_ENABLED" == "true" && "${CERTBOT_HOST}" != "localhost" && -n "${CERTBOT_HOST}" && -n "${CERTBOT_EMAIL}" ]]; then
   echo "Run CertBot for ${CERTBOT_HOST}"
-  certbot certonly --standalone -d ${CERTBOT_HOST} -m ${CERTBOT_EMAIL}
+  certbot certonly --standalone --cert-name=satosa -d ${CERTBOT_HOST} -m ${CERTBOT_EMAIL}
 else
   echo "CertBot skipped"
 fi
+exit 0
