fa99151b17f9c859e39d0966682fbd67c603d9b9
MDreW <andrea.ranaldi@gmail.com>
2025-08-07 14:05:05 +0200

    separata la logica di nginx da certbot

--- Docker-compose/certbot/certbot.sh
+++ Docker-compose/certbot/certbot.sh
@@ -1,6 +1,6 @@
 if [[ "$CERTBOT_ENABLED" == "true" && "${CERTBOT_HOST}" != "localhost" && -n "${CERTBOT_HOST}" && -n "${CERTBOT_EMAIL}" ]]; then
   echo "Run CertBot for ${CERTBOT_HOST}"
-  certbot certonly --standalone --cert-name=satosa -d ${CERTBOT_HOST} -m ${CERTBOT_EMAIL}
+  certbot certonly --standalone --agree-tos --non-interactive -d ${CERTBOT_HOST} -m ${CERTBOT_EMAIL}
 else
   echo "CertBot skipped"
 fi
