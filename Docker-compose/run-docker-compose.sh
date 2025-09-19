804dff50c33137cd6d224c8f2e6d8533b4585ef0
MDreW <andrea.ranaldi@gmail.com>
2025-09-19 10:37:11 +0200

    Added option "-b" to local build iam-proxy-italia image. djangosaml2-sp is compiled only if required

--- Docker-compose/run-docker-compose.sh
+++ Docker-compose/run-docker-compose.sh
@@ -41,12 +41,16 @@ function update {
     docker compose -f docker-compose.yml down -v
     echo -e "\n"
     echo -e "Tiro su la composizione, in caso, con le nuove versioni delle immagini. \n"
-    docker compose -f docker-compose.yml build django_sp
+    # docker compose -f docker-compose.yml build django_sp
   fi
 }
 
 function start {
-  docker compose -f docker-compose.yml up --wait --wait-timeout 60 --remove-orphans
+  if [ "$SATOSA_BUILD" == "true" ]; then
+    docker compose -f docker-compose.yml up --wait --wait-timeout 60 --remove-orphans --build
+  else
+    docker compose -f docker-compose.yml up --wait --wait-timeout 60 --remove-orphans
+  fi
   echo -e "\n"
   echo -e "Completato. Per visionare i logs: 'docker-compose -f docker-compose.yml logs -f'"
   exit 0
@@ -54,30 +58,38 @@ function start {
 
 function help {
   echo ""
-  echo "### run-docker-compose.sh ###"
+  echo "### run-docker-compose.sh"
   echo ""
   echo "initialize check update and start iam-proxy-italia compose structure"
   echo ""
-  echo "Options"
-  echo "-f Force clean and reinitialize data for Satosa, MongoDB and Djangosaml2_SP"
+  echo "#### common Options"
+  echo "-b Build for iam-proxy-italia image and build django-sp image if required"
   echo "-e Force update for .env file. A new .env file is generated from env.example file"
+  echo "-f Force clean and reinitialize data for Satosa, MongoDB and Djangosaml2_SP"
   echo "-h Print this help"
-  echo "-s Skip docker image update"
-  echo "-p unset compose profile. Run: satosa and nginx. Usefull for production"
+  echo ""
+  echo "#### profile options"
   echo "-m Set 'mongo' compose profile. Run: satosa, nginx, mongo"
   echo "-M Set 'mongoexpress' compose profile. Run: satosa, nginx, mongo, mongo-express"
+  echo "-p unset compose profile. Run: satosa and nginx. Usefull for production"
+  echo "-s Skip docker image update"
   echo "-d Set 'dev' compose profile. Run: satosa, nginx, django-sp, spid-saml-check"
-  echo "   if isn't set any of -p, -m, -M, -d, is used 'demo' compose profile"
-  echo "   demo compose profile start: satosa, nginx, mongo, mongo-express, django-sp, spid-saml-check"
+  echo ""
+  echo "if isn't set any options of -p, -m, -M, -d, is used 'demo' compose profile"
+  echo "demo compose profile start: satosa, nginx, mongo, mongo-express, django-sp, spid-saml-check"
+  echo ""
 }
 
-while getopts ":fepimMdsh" opt; do
+while getopts ":fepbimMdsh" opt; do
   case ${opt} in
    f)
      clean_data
      ;;
    e)
      SATOSA_FORCE_ENV="true"
+     ;;
+   b)
+     SATOSA_BUILD="true"
      ;;
    p)
      unset COMPOSE_PROFILES
