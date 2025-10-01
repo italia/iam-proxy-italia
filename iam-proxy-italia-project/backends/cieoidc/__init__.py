ef7fdf431e8c83a6ff757dc77483e05397825d3f
m.ciofo <m.ciofo@ipzs.it>
2025-10-01 10:14:44 +0200

    - Add trust chain configuration and properties

--- iam-proxy-italia-project/backends/cieoidc/__init__.py
+++ iam-proxy-italia-project/backends/cieoidc/__init__.py
@@ -5,6 +5,9 @@ from .tools.endpoints_loader import EndpointsLoader
 from satosa.backends.base import BackendModule
 from satosa.backends.oauth import get_metadata_desc_for_oauth_backend
 from satosa.backends.openid_connect import OpenIDConnectBackend
+from pyeudiw.federation.trust_chain_builder import TrustChainBuilder
+from pyeudiw.federation.statements import EntityStatement, get_entity_configurations
+
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +21,7 @@ class CieOidcBackend(BackendModule):
         super().__init__(callback, internal_attributes, base_url, name)
         self.config = module_config
         self.endpoints = {}
+        self.trust_chain = self._generate_trust_chain()
         # self.auth_callback_func = auth_callback_func
         # self.internal_attributes = internal_attributes
         # self.converter = AttributeMapper(internal_attributes)
@@ -78,3 +82,48 @@ class CieOidcBackend(BackendModule):
         meta = get_metadata_desc_for_oauth_backend(self.config["provider_metadata"]["issuer"], self.config)
         return meta
 
+    def _generate_trust_chain(self):
+        logger.debug(
+            f"Entering method: {inspect.getframeinfo(inspect.currentframe()).function}. "
+        )
+        logger.info(f"[INFO] config: {self.config}")
+
+        httpc_params = self.config["trust_chain"]["config"]["httpc_params"]
+
+        logger.info(f"[INFO] httpc_params: {httpc_params}")
+
+        jwt = get_entity_configurations(self.config["trust_chain"]["config"]["trust_anchor"][0], httpc_params=httpc_params)[0]
+
+        logger.info(f"[INFO] jwt: {jwt}")
+
+
+        jwt_cie = get_entity_configurations("http://cie-provider.org:8002/oidc/op/", httpc_params=httpc_params)[0]
+
+        logger.info(f"[INFO] jwt: {jwt_cie}")
+
+        trust_anchor_ec = EntityStatement(jwt.decode(), httpc_params=httpc_params)
+
+        logger.info(f"[INFO] trust_anchor_ec: {trust_anchor_ec}")
+
+        trust_anchor_ec.validate_by_itself()
+
+        trust_chain = TrustChainBuilder(
+            subject="http://cie-provider.org:8002/oidc/op/",
+            trust_anchor=trust_anchor_ec.sub,
+            trust_anchor_configuration=trust_anchor_ec,
+            httpc_params=httpc_params,
+        )
+
+        logger.info(f"[INFO] trust_chain: {trust_chain}")
+
+        logger.info(f"[INFO] trust_chain_start")
+
+        trust_chain.start()
+
+        logger.info(f"[INFO] apply_metadata_policy")
+
+        trust_chain.apply_metadata_policy()
+
+        logger.info(f"[INFO] return trust_chain")
+
+        return trust_chain
