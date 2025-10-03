f8f658d865b07898f3bf5521124d50f874c80752
sara.longobardi <sara.longobardi@accenture.com>
2025-10-03 17:01:55 +0200

    fix: healtcheck, avoid timeout when one check failure and exit code 1 at first ko + align url with config

--- Docker-compose/pyeudiw-healthcheck.sh
+++ Docker-compose/pyeudiw-healthcheck.sh
@@ -2,26 +2,32 @@
 set -e
 
 PROXY_CONF_PATH="iam-proxy-italia-project/proxy_conf.yaml"
+WGET_OPTS="--timeout=5 --tries=1 --no-check-certificate"
 
-if grep -qF "conf/backends/pyeudiw_backend.yaml" "$PROXY_CONF_PATH"; then
+function check_url {
+  local url=$1
+  if ! wget $WGET_OPTS -O - "$url"; then
+    echo "Error: wget failed for $url"
+    exit 1
+  fi
+}
+
+if grep -qF "./conf/backends/pyeudiw_backend.yaml" "$PROXY_CONF_PATH"; then
   echo "pyeudiw_backend.yaml configured"
-  (
-    wget -O - https://satosa-nginx/OpenID4VCI/.well-known/openid-federation --no-check-certificate &&
-    wget -O - https://satosa-nginx/OpenID4VCI/.well-known/oauth-authorization-server --no-check-certificate &&
-    wget -O - https://satosa-nginx/OpenID4VCI/.well-known/openid-credential-issuer --no-check-certificate
-  ) || exit 1
+  check_url "https://satosa-nginx/OpenID4VP/.well-known/jwt-vc-issuer"
+  check_url "https://satosa-nginx/OpenID4VP/.well-known/jar-issuer"
+  check_url "https://satosa-nginx/OpenID4VP/.well-known/openid-federation"
 fi
 
-if grep -qF "conf/frontends/openid4vci_frontend.yaml" "$PROXY_CONF_PATH"; then
+if grep -qF "./conf/frontends/openid4vci_frontend.yaml" "$PROXY_CONF_PATH"; then
   echo "openid4vci_frontend.yaml configured"
-  (
-    wget -O - https://satosa-nginx/OpenID4VP/.well-known/jwt-vc-issuer --no-check-certificate &&
-    wget -O - https://satosa-nginx/OpenID4VP/.well-known/jar-issuer --no-check-certificate &&
-    wget -O - https://satosa-nginx/OpenID4VP/.well-known/openid-federation --no-check-certificate
-  ) || exit 1
+  check_url "https://satosa-nginx/OpenID4VCI/.well-known/openid-federation"
+  check_url "https://satosa-nginx/OpenID4VCI/.well-known/oauth-authorization-server"
+  check_url "https://satosa-nginx/OpenID4VCI/.well-known/openid-credential-issuer"
 fi
 
-if grep -qF "conf/frontends/saml2_frontend.yaml" "$PROXY_CONF_PATH"; then
+if grep -qF "./conf/frontends/saml2_frontend.yaml" "$PROXY_CONF_PATH"; then
   echo "saml2_frontend.yaml configured"
-  wget -O - https://satosa-nginx/Saml2IDP/metadata --no-check-certificate || exit 1
+  check_url "https://satosa-nginx/Saml2IDP/metadata"
 fi
+echo "Health check completed successfully."
