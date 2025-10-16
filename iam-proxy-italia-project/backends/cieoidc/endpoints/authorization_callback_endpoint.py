c3ae0b57a381dd4e0fc29720bc130057c6ebbb89
manpace <221346462+manpace@users.noreply.github.com>
2025-10-16 13:11:57 +0200

    - fix

--- iam-proxy-italia-project/backends/cieoidc/endpoints/authorization_callback_endpoint.py
+++ iam-proxy-italia-project/backends/cieoidc/endpoints/authorization_callback_endpoint.py
@@ -286,7 +286,7 @@ class AuthorizationCallBackHandler(BaseEndpoint):
         return self.auth_callback_func(context, "")
 
 
-    def __get_authorization(self, state: str):
+    def __get_authorization(self, state: str) -> dict:
 
         """
         method __get_authorization:
@@ -299,25 +299,14 @@ class AuthorizationCallBackHandler(BaseEndpoint):
         :param state: str
 
         """
-
-        logger.debug(
-            f"Entering method: {inspect.getframeinfo(inspect.currentframe()).function}. Params [state {state}]"
-        )
-
+        logger.debug(f"Entering method: {inspect.getframeinfo(inspect.currentframe()).function}. Params [state {state}]")
         try:
-
-
-            result = self._repo_auth_callback.find_all({"state": state})
-
-            logger.debug(
-                f"Found result: {result}"
-            )
+            output = self._repo_auth_callback.find_all({"state": state})
+            if output:
+                return output[0].model_dump(mode="json")
         except ValidationError as e:
             logger.debug(e)
-
-        output = {'client_id': 'http://relying-party.org:8001', 'state': 'kTN6Rb83bJEMjKakV1DTlVh9xpSTyQOm', 'endpoint': 'http://cie-provider.org:8002/oidc/op/authorization', 'provider_id': 'http://cie-provider.org:8002/oidc/op/', 'data': '{"iss": null, "scope": "openid", "redirect_uri": "http://relying-party.org:8001/oidc/rp/callback", "response_type": "code", "nonce": "XGJNw0R2poSWEC3VAabCJVu6m6mOBhOI", "state": "kTN6Rb83bJEMjKakV1DTlVh9xpSTyQOm", "client_id": null, "endpoint": "http://cie-provider.org:8002/oidc/op/authorization", "acr_values": "https://www.spid.gov.it/SpidL2", "iat": 1759501758, "exp": 1759501818, "jti": "f8b1dee2-b3e4-4e7a-a1f7-9499642164a9", "aud": "http://cie-provider.org:8002/oidc/op/authorization", "claims": {"id_token": {"family_name": {"essential": true}, "given_name": {"essential": true}}, "userinfo": {"given_name": null, "family_name": null, "email": null, "https://attributes.eid.gov.it/fiscal_number": null}}, "prompt": "consent login", "code_verifier": "TD7mCHoId-sCz8eMK.YQtx6Lv~yVqxE6RkI14JPFORWUNBtcQ2hG1ubG-5QgaiOL_Wkxk7WE-a4VrzN9D8V~NgooPGTjwbjmtgM_3H_MyOqVCN1Iqd", "code_challenge": "7w-ZWqS9uuuL5LnRZj26yYJB6EX_E9z8zvKOK_nIZIA", "code_challenge_method": "S256"}', 'provider_configuration': {'federation_entity': {'federation_resolve_endpoint': 'http://cie-provider.org:8002/oidc/op/resolve', 'organization_name': 'CIE OIDC identity provider', 'homepage_uri': 'http://cie-provider.org:8002', 'policy_uri': 'http://cie-provider.org:8002/oidc/op/en/website/legal-information', 'logo_uri': 'http://cie-provider.org:8002/static/svg/logo-cie.svg', 'contacts': ['tech@example.it']}, 'openid_provider': {'authorization_endpoint': 'http://cie-provider.org:8002/oidc/op/authorization', 'revocation_endpoint': 'http://cie-provider.org:8002/oidc/op/revocation', 'id_token_encryption_alg_values_supported': ['RSA-OAEP'], 'id_token_encryption_enc_values_supported': ['A128CBC-HS256'], 'token_endpoint': 'http://cie-provider.org:8002/oidc/op/token', 'userinfo_endpoint': 'http://cie-provider.org:8002/oidc/op/userinfo', 'introspection_endpoint': 'http://cie-provider.org:8002/oidc/op/introspection', 'claims_parameter_supported': True, 'contacts': ['ops@https://idp.it'], 'code_challenge_methods_supported': ['S256'], 'client_registration_types_supported': ['automatic'], 'request_authentication_methods_supported': {'ar': ['request_object']}, 'acr_values_supported': ['https://www.spid.gov.it/SpidL1', 'https://www.spid.gov.it/SpidL2', 'https://www.spid.gov.it/SpidL3'], 'claims_supported': ['given_name', 'family_name', 'birthdate', 'gender', 'phone_number', 'https://attributes.eid.gov.it/fiscal_number', 'phone_number_verified', 'email', 'address', 'document_details', 'https://attributes.eid.gov.it/physical_phone_number'], 'grant_types_supported': ['authorization_code', 'refresh_token'], 'id_token_signing_alg_values_supported': ['RS256', 'ES256'], 'issuer': 'http://cie-provider.org:8002/oidc/op', 'jwks_uri': 'http://cie-provider.org:8002/oidc/op/openid_provider/jwks.json', 'signed_jwks_uri': 'http://cie-provider.org:8002/oidc/op/openid_provider/jwks.jose', 'jwks': {'keys': [{'kty': 'RSA', 'use': 'sig', 'e': 'AQAB', 'n': 'rJoSYv1stwlbM11tR9SYGIJuzqlJe2bv2N35oPRbwV_epjNWvGG2ZqEj53YFMC8AMZNFhuLa_LNwr1kLVE-jXQe8xjiLhe7DgMf1OnSzq9yAEXVo19BPBwkgJe2jp9HIgM_nfbIsUbSSkFAM2CKvGb0Bk2GvvqXZ12P-fpbVyA9hIQr6rNTqnCGx2-v4oViGG4u_3iTw7D1ZvLWmrmZOaKnDAqG3MJSdQ-2ggQ-Aiahg48si9C9D_JgnBV9tJ2eCS58ZC6kVG5sftElQVdH6e26mz464TZj5QgCwZCTsAQfIvBoXSdCKxpnvsFfrajz4q9BiXAryxIOl5fLmCFVNhw', 'kid': 'Pd2N9-TZz_AWS3GFCkoYdRaXXls8YPhx_d_Ez7JwjQI'}]}, 'scopes_supported': ['openid', 'offline_access'], 'logo_uri': 'http://cie-provider.org:8002/static/images/logo-cie.png', 'organization_name': 'SPID OIDC identity provider', 'op_policy_uri': 'http://cie-provider.org:8002/oidc/op/en/website/legal-information', 'request_parameter_supported': True, 'request_uri_parameter_supported': True, 'require_request_uri_registration': True, 'response_types_supported': ['code'], 'response_modes_supported': ['query', 'form_post'], 'subject_types_supported': ['pairwise', 'public'], 'token_endpoint_auth_methods_supported': ['private_key_jwt'], 'token_endpoint_auth_signing_alg_values_supported': ['RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512'], 'userinfo_encryption_alg_values_supported': ['RSA-OAEP', 'RSA-OAEP-256'], 'userinfo_encryption_enc_values_supported': ['A128CBC-HS256', 'A192CBC-HS384', 'A256CBC-HS512', 'A128GCM', 'A192GCM', 'A256GCM'], 'userinfo_signing_alg_values_supported': ['RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512'], 'request_object_encryption_alg_values_supported': ['RSA-OAEP', 'RSA-OAEP-256'], 'request_object_encryption_enc_values_supported': ['A128CBC-HS256', 'A192CBC-HS384', 'A256CBC-HS512', 'A128GCM', 'A192GCM', 'A256GCM'], 'request_object_signing_alg_values_supported': ['RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512']}}}
-
-        return output
+        return {}
 
     def __create_token(self, authorization_input: dict, code: str) -> dict:
 
