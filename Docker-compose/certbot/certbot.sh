08b9ececa7e9acd4e14780a910f4a131a171f236
m.ciofo <m.ciofo@ipzs.it>
2026-03-03 17:08:06 +0100

    Update certbot and environment for certificate. remove usless JS and update README with pem generation for pyeudiw

--- Docker-compose/certbot/certbot.sh
+++ Docker-compose/certbot/certbot.sh
@@ -1,29 +1,29 @@
 case "$CERT_METHOD" in
   certbot)
-    if [[ "$CERTBOT_HOST" == "localhost" || -z "$CERTBOT_HOST" || -z "$CERTBOT_EMAIL" ]]; then
+    if [[ "$HOSTNAME" == "localhost" || -z "$HOSTNAME" || -z "$CERTBOT_EMAIL" ]]; then
       echo "Invalid CERTBOT configuration"
       exit 1
     fi
 
-    echo "Run CertBot for ${CERTBOT_HOST}"
+    echo "Run CertBot for ${HOSTNAME}"
     certbot certonly --standalone --agree-tos --non-interactive \
-      -d "$CERTBOT_HOST" \
+      -d "$HOSTNAME" \
       -m "$CERTBOT_EMAIL"
     ;;
 
   local)
-    if [[ -z "$CERTBOT_HOST" ]]; then
-      echo "CERTBOT_HOST not set"
+    if [[ -z "$HOSTNAME" ]]; then
+      echo "HOSTNAME not set"
       exit 1
     fi
 
-    mkdir -p "/etc/letsencrypt/live/${CERTBOT_HOST}"
+    mkdir -p "/etc/letsencrypt/live/${HOSTNAME}"
 
     openssl req -x509 -nodes -days 3650 \
       -newkey rsa:3072 \
-      -keyout "/etc/letsencrypt/live/${CERTBOT_HOST}/privkey.pem" \
-      -out "/etc/letsencrypt/live/${CERTBOT_HOST}/fullchain.pem" \
-      -subj "/CN=${CERTBOT_HOST}"
+      -keyout "/etc/letsencrypt/live/${HOSTNAME}/privkey.pem" \
+      -out "/etc/letsencrypt/live/${HOSTNAME}/fullchain.pem" \
+      -subj "/CN=${HOSTNAME}"
     ;;
 
   *)
